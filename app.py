b5eb12ad16882399bcb05775b1f52fe96d2fc951
Lev Korotich <51894137+realar@users.noreply.github.com>
2024-12-07 16:44:52 +1000

    Update app.py

--- app.py
+++ app.py
@@ -18,5 +18,21 @@ def webhook():
         dispatcher.process_update(update)
     return "OK", 200
 
+@app.route('/')
+def index():
+    return render_template('index.html', title="Каталог компаний")
+
+@app.route('/autos')
+def autos():
+    return render_template('autos.html', title="Авто в наличии")
+
+@app.route('/calc')
+def calc():
+    return render_template('calc.html', title="Калькулятор")
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="О сервисе")
+
 if __name__ == '__main__':
     app.run(host=HOST, port=PORT)
